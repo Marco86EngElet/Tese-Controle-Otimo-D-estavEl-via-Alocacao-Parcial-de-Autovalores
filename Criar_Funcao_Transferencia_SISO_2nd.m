7b41a1a850bc3dde5b05ae859699e2c1e6a3e623
Marco86EngElet <50562687+Marco86EngElet@users.noreply.github.com>
2025-05-25 18:24:21 -0300

    Delete Criar_Funcao_Transferencia_SISO_2nd.m

--- Criar_Funcao_Transferencia_SISO_2nd.m
+++
@@ -1,6 +0,0 @@
-function H = Criar_Funcao_Transferencia_SISO_2nd(K, zeta, omega_n)
-    % Criar a funcao de transferencia de segunda ordem
-    num = K * omega_n^2; % Numerador
-    den = [1, 2*zeta*omega_n, omega_n^2]; % Denominador
-    H = tf(num, den); % Criar a funcao de transferencia
-end
