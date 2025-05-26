8cce5c1f300166983839703fa4f7ece4389da552
Marco86EngElet <50562687+Marco86EngElet@users.noreply.github.com>
2025-05-26 13:24:03 -0300

    Update Computar_Dados_Simulacao_impulso.m
    Reescrito os comentários

--- Computar_Dados_Simulacao_impulso.m
+++ Computar_Dados_Simulacao_impulso.m
@@ -1,8 +1,8 @@
-%11.1) Sinal y(t), x(t) e o 't' associado a d(t) impulso
+%A) Sinal y(t), x(t) e o 't' associado a d(t) impulso
     
     [y_Canon_Godys_impulse,t_Canon_Godys_impulse,x_Canon_Godys_impulse]=...
         impulse(Canon_Godys,tempo_simulacao_impulso);
     
-    %11.2) Sinal (y(t))^2 associado a d(t) impulso
+%B) Sinal (y(t))^2 associado a d(t) impulso
     
     y2_Canon_Godys_impulse=y_Canon_Godys_impulse.*y_Canon_Godys_impulse;
