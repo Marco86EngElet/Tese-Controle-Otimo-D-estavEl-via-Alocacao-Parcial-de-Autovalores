77f7c3543134609d2ee3b3fe6958fb5a0e21e696
Marco86EngElet <50562687+Marco86EngElet@users.noreply.github.com>
2025-05-26 13:26:08 -0300

    Update Computar_Medidas_Transitoria_Sistema_Malha_Aberta.m
    Reescrita de comentários

--- Computar_Medidas_Transitoria_Sistema_Malha_Aberta.m
+++ Computar_Medidas_Transitoria_Sistema_Malha_Aberta.m
@@ -1,50 +1,50 @@
-%% 9) Computar as medidas transitorias para as funcoes parciais         
+%% A) Computar as medidas transitorias para as funcoes parciais         
 
     for c=1:n_x %Laco para computar medidas transitoria para cada fracao
         
-        %9.1) Maximo Sobressinal para fracoes parciais com polos complexos
+        %A.1) Maximo Sobressinal para fracoes parciais com polos complexos
     
         Mos_God(c,1)=exp(-pi*zeta_God(c,1)/(sqrt(1-zeta_God(c,1)^2)));
         
-        %9.2) Tempo de Acomodacao para fracoes parciais com polos complexos
+        %A.2) Tempo de Acomodacao para fracoes parciais com polos complexos
         
         cs = 0.05;
         Ts_God(c,1)=...
             -log(cs*sqrt(1-zeta_God(c,1)^2))/(wn_God(c,1)*zeta_God(c,1));
     
-        %9.3) Tempo de Atraso para fracoes parciais com polos complexos
+        %A.3) Tempo de Atraso para fracoes parciais com polos complexos
         
         Td_God(c,1)=...
             (1.1+0.123*zeta_God(c,1)+0.495*zeta_God(c,1)^2)/(wn_God(c,1));
     
-        %9.4) Tempo de Subida para fracoes parciais com polos complexos
+        %A.4) Tempo de Subida para fracoes parciais com polos complexos
         
         Tr_God(c,1)=...
             (1.-0.416*zeta_God(c,1)+2.917*zeta_God(c,1)^2)/(wn_God(c,1));
     
-        %9.5) Tempo de Pico para fracoes parciais com polos complexos
+        %A.5) Tempo de Pico para fracoes parciais com polos complexos
         
         Tp_God(c,1)=pi/(wn_God(c,1)*sqrt(1-zeta_God(c,1)^2));
     
     end
     
-%% 10) Construir Tabela para Verificar medidas transitoria associadas aos 
+%% B) Construir Tabela para Verificar medidas transitoria associadas aos 
 %     polos complexos conjugados
     
-    %10.1) Polos escritos na forma de numero complexo
+    %B.1) Polos escritos na forma de numero complexo
     
     Polos=...
         zeta_God.*wn_God+1i*wn_God.*sqrt(1-zeta_God.*zeta_God);
     
-    %10.2) Frequencia natural dos polos selecionados
+    %B.2) Frequencia natural dos polos selecionados
 
     Wn=wn_God;
 
-    %10.3) Coeficiente de Amortecimento dos polos selecionados
+    %B.3) Coeficiente de Amortecimento dos polos selecionados
 
     Zeta=zeta_God;
     
-    %10.4) Maximo Sobresinal polos selecionados
+    %B.4) Maximo Sobresinal polos selecionados
     
     Mos=Mos_God;
     
@@ -54,7 +54,7 @@
         Mos_Ok=NaN(size(Mos));
     end
     
-    %10.5) Tempo de acomodacao polos selecionados
+    %B.5) Tempo de acomodacao polos selecionados
     
     Ts=Ts_God;
 
@@ -72,7 +72,7 @@
        end
     end
     
-    %10.6) Tempo de atraso polos selecionados
+    %B.6) Tempo de atraso polos selecionados
     
     Td=Td_God;
     
@@ -82,7 +82,7 @@
         Td_Ok=NaN(size(Td));
     end
 
-    %10.7) Tempo de subida polos selecionados
+    %B.7) Tempo de subida polos selecionados
 
     Tr=Tr_God;
 
@@ -100,7 +100,7 @@
         end
     end
     
-    %10.8) Tempo de pico polos selecionados
+    %B.8) Tempo de pico polos selecionados
     
     Tp=Tp_God;
 
@@ -110,7 +110,7 @@
         Tp_Ok=NaN(size(Tp));
     end
 
-    %10.9 Construir Tabelas relacionado Polos selecionados e medidas
+    %B.9 Construir Tabelas relacionado Polos selecionados e medidas
     %transitorias associadas ao projeto de alocacao parcial otima
     %multietapa
     
