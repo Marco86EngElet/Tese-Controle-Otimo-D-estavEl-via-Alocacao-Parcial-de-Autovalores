b14721f83da5ec99b5b5322f5d6e38d3585bd324
Marco86EngElet <50562687+Marco86EngElet@users.noreply.github.com>
2025-05-26 13:32:07 -0300

    Update Computar_Medidas_Transitorias_Malha_Fechada_via_parcial.m
    Reescrito os comentários.

--- Computar_Medidas_Transitorias_Malha_Fechada_via_parcial.m
+++ Computar_Medidas_Transitorias_Malha_Fechada_via_parcial.m
@@ -2,41 +2,41 @@ for i=1:size(p_theo7,1)
 
     if zeta_theo7(i)<1
         
-        %32.1) Maximo Sobressinal para fracoes parciais com polos complexos
+        %A.1) Maximo Sobressinal para fracoes parciais com polos complexos
         Mos_Gcd_parcial(i,1)=exp(-pi*zeta_theo7(i,1)/(sqrt(1-zeta_theo7(i,1)^2)));
 
-        %32.2) Tempo de Acomodacao para fracoes parciais com polos complexos
+        %A.2) Tempo de Acomodacao para fracoes parciais com polos complexos
         cs = 0.05;
         Ts_Gcd_parcial(i,1) = -log(cs*sqrt(1-zeta_theo7(i,1)^2))/(wn_theo7(i,1)*zeta_theo7(i,1));
 
-        %32.3) Tempo de Atraso para fracoes parciais com polos complexos
+        %A.3) Tempo de Atraso para fracoes parciais com polos complexos
         Td_Gcd_parcial(i,1) = (1.1+0.123*zeta_theo7(i,1)+0.495*zeta_theo7(i,1)^2)/(wn_theo7(i,1));
 
-        %32.4) Tempo de Subida para fracoes parciais com polos complexos
+        %A.4) Tempo de Subida para fracoes parciais com polos complexos
         Tr_Gcd_parcial(i,1) = (1.-0.416*zeta_theo7(i,1)+2.917*zeta_theo7(i,1)^2)/(wn_theo7(i,1));
 
-        %32.5) Tempo de Pico para fracoes parciais com polos complexos
+        %A.5) Tempo de Pico para fracoes parciais com polos complexos
         Tp_Gcd_parcial(i,1) = pi/(wn_theo7(i,1)*sqrt(1-zeta_theo7(i,1)^2));
         
     else
         if zeta_theo7(i)==1
-            %32.6) Maximo Sobressinal para fracoes parciais com polo real
+            %A.6) Maximo Sobressinal para fracoes parciais com polo real
             Mos_Gcd_parcial(i,1)=0;
 
-            %32.7) Tempo de Acomodacao para fracoes parciais com polo real
+            %A.7) Tempo de Acomodacao para fracoes parciais com polo real
             Ts_Gcd_parcial(i,1) = 5/wn_theo7(i);
 
-            %32.8) Tempo de Atraso para fracoes parciais com polo real
+            %A.8) Tempo de Atraso para fracoes parciais com polo real
             Td_Gcd_parcial(i,1) = NaN(1);
 
-            %32.9) Tempo de Subida para fracoes parciais com polo real
+            %A.9) Tempo de Subida para fracoes parciais com polo real
             Tr_Gcd_parcial(i,1) = 2.2/wn_theo7(i);
 
-            %32.10) Tempo de Pico para fracoes parciais com polo real
+            %A.10) Tempo de Pico para fracoes parciais com polo real
             Tp_Gcd_parcial(i,1) = NaN(1);
         
         else
-            %32.11) Polos instaveis
+            %A.11) Polos instaveis
             Mos_Gcd_parcial(i,1)=NaN(1);
             Ts_Gcd_parcial(i,1) = NaN(1);
             Td_Gcd_parcial(i,1) = NaN(1);
@@ -46,23 +46,23 @@ for i=1:size(p_theo7,1)
     end
 end
 
-%% 33) Construir Tabela para Mostrar Polos e Medidas Transitorias Associados
+%% B) Construir Tabela para Mostrar Polos e Medidas Transitorias Associados
 %    do sistema LCTI-MIMO-CRPE projetado via metodo parcial otimo com LMIs.
 
-    %33.1) Selecionando apenas um elemento de cada par de polos complexos
+    %B.1) Selecionando apenas um elemento de cada par de polos complexos
     %conjugados
 
     Polos=p_theo7;
 
-    %33.2) Frequencia natural dos polos selecionados
+    %B.2) Frequencia natural dos polos selecionados
 
     Wn=wn_theo7;
 
-    %33.3) Coeficiente de Amortecimento dos polos selecionados
+    %B.3) Coeficiente de Amortecimento dos polos selecionados
 
     Zeta=zeta_theo7;
 
-    %33.4) Maximo Sobresinal polos selecionados
+    %B.4) Maximo Sobresinal polos selecionados
     
     Mos=Mos_Gcd_parcial;
     
@@ -72,7 +72,7 @@ end
         Mos_Ok=NaN(size(Mos));
     end
     
-    %33.5) Tempo de acomodacao polos selecionados
+    %B.5) Tempo de acomodacao polos selecionados
     
     Ts=Ts_Gcd_parcial;
 
@@ -90,7 +90,7 @@ end
        end
     end
     
-    %33.6) Tempo de atraso polos selecionados
+    %B.6) Tempo de atraso polos selecionados
     
     Td=Td_Gcd_parcial;
 
@@ -100,7 +100,7 @@ end
         Td_Ok=NaN(size(Td));
     end
     
-    %33.7) Tempo de subida polos selecionados
+    %B.7) Tempo de subida polos selecionados
 
     Tr=Tr_Gcd_parcial;
 
@@ -118,7 +118,7 @@ end
         end
     end
     
-    %33.8) Tempo de pico polos selecionados
+    %B.8) Tempo de pico polos selecionados
     
     Tp=Tp_Gcd_parcial;
 
@@ -128,7 +128,7 @@ end
         Tp_Ok=NaN(size(Tp));
     end
     
-    %33.9) Construir Tabelas relacionado Polos selecionados e medidas
+    %B.9) Construir Tabelas relacionado Polos selecionados e medidas
     %transitorias associadas ao projeto de alocacao parcial otima
     %multietapa
     
