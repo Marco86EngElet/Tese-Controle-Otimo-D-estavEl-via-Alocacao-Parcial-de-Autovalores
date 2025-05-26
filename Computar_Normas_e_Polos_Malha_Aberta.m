3585fb7e059ac3012c82bc140830103d4f069699
Marco86EngElet <50562687+Marco86EngElet@users.noreply.github.com>
2025-05-26 13:32:41 -0300

    Update Computar_Normas_e_Polos_Malha_Aberta.m
    Reescrito os comentários

--- Computar_Normas_e_Polos_Malha_Aberta.m
+++ Computar_Normas_e_Polos_Malha_Aberta.m
@@ -1,13 +1,13 @@
-%% 7) Computar as Normas "H_2" e "H_infinito" do sistema em malha aberta     
+%% A) Computar as Normas "H_2" e "H_infinito" do sistema em malha aberta     
 
-    %7.1) Norma H_2 do sistema canonico controlavel de Gody(s)
+    %A.1) Norma H_2 do sistema canonico controlavel de Gody(s)
     
     norma_H2_Godys=norm(Canon_Godys,2);
     
-    %7.2) Norma H_infinito do sistema canonico controlavel de Godz(s)
+    %A.2) Norma H_infinito do sistema canonico controlavel de Godz(s)
     
     norma_Hinf_Godzs=norm(Canon_Godzs,'inf');
     
-%% 8) Computar Os polos do sistema em malha aberta 
+%% B) Computar Os polos do sistema em malha aberta 
     
     Polos_Malha_Aberta=cplxpair(pole(Canon_Godys));
